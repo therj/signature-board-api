ab4ad8e707ef1056344d3c051b2f76deffb64be8
Rabindra Joshi <therj@users.noreply.github.com>
2018-07-09 00:02:54 +0545

    move schema, fix endpoints.

--- server.js
+++ server.js
@@ -1,65 +1,70 @@
 require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose');
-const Signature = require('./models/signature.js')
+const mongoose = require('mongoose')
 const cors = require('cors')
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require('body-parser')
 
-(async function () {
-    try {
-        const client = await mongoose.connect(process.env.DATABASE, {
-            useNewUrlParser: true
-        });
-    } catch (e) {
-        console.error("Error connecting to database!")
-    }
+const signatureSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+})
+const Signature = mongoose.model('Signature', signatureSchema)
+;(async function() {
+  try {
+    await mongoose.connect(
+      process.env.DATABASE,
+      { useNewUrlParser: true },
+    )
+  } catch (e) {
+    console.error('Error connecting to database!')
+  }
 })()
 
 const app = express()
 app.use(cors())
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.json('Keep Going!');
-});
-
-app.get('/api/messages', (req, res) => {
-    (async function () {
-        try {
-            const results = await Signature.find({});
-            res.json(results)
-            console.log(results);
-        } catch (err) {
-            throw err;
-        }
-    })()
-
+  res.json('Keep Going!')
 })
 
+app.get('/api/messages', (req, res) => {
+  ;(async function() {
+    try {
+      const results = await Signature.find({})
+      res.json(results)
+    } catch (err) {
+      throw err
+    }
+  })()
+})
 
 app.post('/api/messages', (req, res) => {
-    const {
-        name,
-        message
-    } = req.body
-    if (name.trim() || message.trim()) {
-        return res.status(422).send("Name and message required!")
-    }
+  let { name, message } = req.body
+  if (name && message) {
+    name = name.trim()
+    message = message.trim()
+  } else {
+    return res.status(422).send('Name and message required!')
+  }
 
-    const newSignature = new Signature({
-        "name": name,
-        "message": message
-    })
-    newSignature.save(err => {
-        if (err) return res.status(500).send(err);
-        return res.status(200).send(newSignature);
-    });
-});
-
+  const newSignature = new Signature({
+    name: name,
+    message: message,
+  })
+  newSignature.save((err) => {
+    if (err) return res.status(500).send('Error saving your message!')
+    return res.status(200).send(newSignature)
+  })
+})
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server  running on port ${process.env.PORT}`);
-
+  console.log(`Server  running on port ${process.env.PORT}`)
 })
